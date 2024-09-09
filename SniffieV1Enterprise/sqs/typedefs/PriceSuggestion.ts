92307686a34b7ad9510f30ac84974994e462549a
jukkaovermark <106663188+jukkaovermark@users.noreply.github.com>
2024-09-09 16:15:49 +0300

    add strategyName

--- SniffieV1Enterprise/sqs/typedefs/PriceSuggestion.ts
+++ SniffieV1Enterprise/sqs/typedefs/PriceSuggestion.ts
@@ -1,6 +1,7 @@
 export interface PriceSuggestion {
   sku: string; // sku of the product in Sniffie
   strategyId: string; // id of the strategy in Sniffie
+  strategyName: string; // Name of the strategy in Sniffie
   originalPrice: number; // normal price or compare at price if discounted
   campaignPrice: number | null; // price if discounted
   validFrom: string; // ISO datetime string
