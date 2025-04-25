67ec6bec0fde4978f5d621e73592b524499d48ab
Joonas Sirén <joonas.siren@sniffie.fi>
2025-04-25 14:53:44 +0300

    add tmp to gitignore

--- PricenV1Enterprise/bulkTransfer/index.ts
+++ PricenV1Enterprise/bulkTransfer/index.ts
@@ -5,8 +5,8 @@ const data = {
   and: "rest of the values",
 };
 const environment = "production"; // or "staging"
-const accountId = "replaceMeWithYourAccountId"; // you get these from Pricen
-const token = "replaceMeWithYourToken"; // you get these from Pricen
+const accountId = "2489"; // you get these from Pricen
+const token = "2489&&OggsDcY4y0roZtO6YRRHdcX51JBo16zajo40FUnCGK1GWSPiinK6hUo0YRFFap"; // you get these from Pricen
 getSignedUrlAndPost(accountId, token, data, environment).then((response) => {
   console.log(response);
 });
